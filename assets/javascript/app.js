f3a2cc7dac42eb4920d2217a11dfaa597b59fd6d
blnicholson <blnicholson13@gmail.com>
2018-07-07 12:16:03 -0500

    inital game files

---
+++ assets/javascript/app.js
@@ -0,0 +1,24 @@
+//start button to start the game
+// variables needed
+// win function
+//loss function
+//unanswered function
+
+//variables
+var start;
+var timer = 35;
+var correctCounter=0;
+var incorrectCounter=0;
+var unansweredCounter=0;
+var questions=[];
+var questionOptions=[]
+var answers=[];
+
+function start(){
+    correctCounter=0;
+    incorrectCounter=0;
+    unansweredCounter=0;
+    timer =35;
+
+}
+
