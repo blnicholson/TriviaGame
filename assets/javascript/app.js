8955125769cf71914210ab9fa688801106877372
blnicholson <blnicholson13@gmail.com>
2018-07-13 23:36:50 -0500

    fixed missing gif final game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -40,7 +40,7 @@ var triviaQuestions = [{
     question: "What color is Link's tunic in The Legend Of Zelda?",
     answerChoices: ["Purple", "Teal", "Green"],
     correctAnswer: "Green",
-    image: "assets/images/8bitLink.gif"
+    image: "assets/images/8bitlink.gif"
 
 }, {
     question: "In the Legend of Zelda, Link starts out with how many hearts?",
